8ff071ab0ba45362e41ea1da700051bf92832ad5
Hsiang-Huang Wu <virtuoso.wu@gmail.com>
2021-06-19 00:00:21 -0700

    add creator for target anchors, proposal, and target proposals

--- rpn.py
+++ rpn.py
@@ -1,10 +1,10 @@
 import numpy as np
 from torch.nn import functional as F
-import torch as t
+import torch
 from torch import nn
 
-from model.utils.bbox_tools import generate_anchor_base
-from model.utils.creator_tool import ProposalCreator
+from torchinfo import summary
+
 
 
 class RegionProposalNetwork(nn.Module):
@@ -32,44 +32,46 @@ class RegionProposalNetwork(nn.Module):
 
 
         self.feat_stride = feat_stride
-        self.proposal_layer = ProposalCreator(self, **proposal_creator_params)
-        n_anchor = self.base_anchors.shape[0]
-        self.conv1 = nn.Conv2d(in_channels,  mid_channels, 3, 1, 1)
-        self.score = nn.Conv2d(mid_channels, n_anchor * 2, 1, 1, 0)
-        self.loc   = nn.Conv2d(mid_channels, n_anchor * 4, 1, 1, 0)
+        #self.proposal_layer = ProposalCreator(self, **proposal_creator_params)
+        self.n_anchor = self.base_anchors.shape[0]
+        self.conv1   = nn.Conv2d(in_channels,  mid_channels, 3, 1, 1)
+        self.relu    = nn.ReLU()
+        self.score   = nn.Conv2d(mid_channels, self.n_anchor * 2, 1, 1, 0)
+        self.loc     = nn.Conv2d(mid_channels, self.n_anchor * 4, 1, 1, 0)
 
     def forward(self, x, img_size, scale=1.):
         n, _, hh, ww = x.shape
         anchor = _generated_all_anchors(hh*self.feat_stride, ww*self.feat_stride)
 
-        n_anchor = anchor.shape[0] // (hh * ww)
-        h = F.relu(self.conv1(x))
+        x = self.conv1(x)
+        x = self.relu(x)
 
-        rpn_locs = self.loc(h)
-        # UNNOTE: check whether need contiguous
-        # A: Yes
+        # location regression
+        rpn_locs = self.loc(x)
         rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)
-        rpn_scores = self.score(h)
+
+        # fg/bg classification
+        rpn_scores = self.score(x)
         rpn_scores = rpn_scores.permute(0, 2, 3, 1).contiguous()
-        rpn_softmax_scores = F.softmax(rpn_scores.view(n, hh, ww, n_anchor, 2), dim=4)
+        rpn_softmax_scores = F.softmax(rpn_scores.view(n, hh, ww, self.n_anchor, 2), dim=4)
         rpn_fg_scores = rpn_softmax_scores[:, :, :, :, 1].contiguous()
         rpn_fg_scores = rpn_fg_scores.view(n, -1)
         rpn_scores = rpn_scores.view(n, -1, 2)
 
         rois = list()
         roi_indices = list()
-        for i in range(n):
-            roi = self.proposal_layer(
-                rpn_locs[i].cpu().data.numpy(),
-                rpn_fg_scores[i].cpu().data.numpy(),
-                anchor, img_size,
-                scale=scale)
-            batch_index = i * np.ones((len(roi),), dtype=np.int32)
-            rois.append(roi)
-            roi_indices.append(batch_index)
+        #for i in range(n):
+        #    roi = self.proposal_layer(
+        #        rpn_locs[i].cpu().data.numpy(),
+        #        rpn_fg_scores[i].cpu().data.numpy(),
+        #        anchor, img_size,
+        #        scale=scale)
+        #    batch_index = i * np.ones((len(roi),), dtype=np.int32)
+        #    rois.append(roi)
+        #    roi_indices.append(batch_index)
 
-        rois = np.concatenate(rois, axis=0)
-        roi_indices = np.concatenate(roi_indices, axis=0)
+        #rois = np.concatenate(rois, axis=0)
+        #roi_indices = np.concatenate(roi_indices, axis=0)
         return rpn_locs, rpn_scores, rois, roi_indices, anchor
 
     def _generated_all_anchor(height, width):
@@ -89,4 +91,5 @@ class RegionProposalNetwork(nn.Module):
         anchor = anchor.reshape((K * A, 4))
         return anchor
 
-
+rpn_inst = RegionProposalNetwork()
+summary(rpn_inst)
